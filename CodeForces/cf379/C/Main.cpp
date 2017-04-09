3fc13f279000e668ff9fce25370cccc9fb136717
Texas-C <616041132@qq.com>
2017-04-09 22:14:27 +0800

    Added cf379

--- CodeForces/cf379/C/Main.cpp
+++ CodeForces/cf379/C/Main.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 const int MAXN = 3e5 + 5;
 typedef pair<int, int> Point;
+
 int lib[MAXN];
 
-map<int, vector<int> > mps;
-map<int, vector<int> > :: iterator it;
-map<int, vector<int> > :: iterator it2;
-vector<int> :: iterator it_v;
+Point mps[MAXN];
 
 void solve()
 {
@@ -17,53 +15,17 @@ void solve()
 	for(int i = 0; i < n; ++i)
 	{
 		scanf("%d", lib + i);
-		mps[ lib[i] ].push_back( i );
+		mps[i].first = lib[i];
+		mps[i].second = i;
 	}
 
 	k = 0;
-	while( mps.begin() != mps.end())
-	{
-		it = mps.begin();
-		if( it->second.size() > 1)
-		{
-			if( k <= it->first )
-				k = it->first + 1;
-
-			it2 = it;
-			++it2;
-
-			while( it2 != mps.end() &&  k == it2->first )
-			{
-				++k;
-				++it2;
-			}
-
-			cnt = it->second.size();
-
-			it_v = it->second.begin();
-			++it_v;
-
-			for(; it_v != it->second.end(); ++it_v)
-			{
-				lib [*it_v] = k;
-//				mps[k].push_back( *it_v );
-				++k;
-
-				while( it2 != mps.end() && k == it2->first )
-				{
-					++k;
-					++it2;
-				}
-			}
-		}
-		mps.erase( it );
-	}
 
 	for(int i = 0; i < n; ++i)
 	{
-//		if( i )
-//			putchar (' ');
-		printf("%d\n", lib[i]);
+		if( i )
+			putchar (' ');
+		printf("%d", lib[i]);
 	}
 	printf("\n");
 }
