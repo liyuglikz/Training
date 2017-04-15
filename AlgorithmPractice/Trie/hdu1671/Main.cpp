52c8027704f983ef4cb48aa3d222da69f85e1abb
Texas-C <616041132@qq.com>
2017-04-15 20:23:34 +0800

    Accepted hdu1671

--- AlgorithmPractice/Trie/hdu1671/Main.cpp
+++ AlgorithmPractice/Trie/hdu1671/Main.cpp
@@ -7,40 +7,100 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <map>
+#include <stack>
+
 using namespace std;
+const int MAXN = 1e4 + 5;
 const int MAXL = 26;
+const int MAXM = 1e8 + 5;
 
 class Node
 {
 	public:
-		Node(){	memset( next, 0, sizeof(next));}
-		Node *next[MAXL];
-		bool flags;
-};
+		Node() : cnt(0){	next.clear();}
+		map<int, Node*> next;
+		int cnt;
+} *root;
+char s[MAXN][MAXL];
 
 void insertNode( char *str )
 {
 	int now;
-	Node *p;
+	Node *p = root;
 	
 	while( *str )
 	{
 		now = *str - '0';
-
-		if( p->next[now] )
-		{
+		if( NULL == p->next[now] )
 			p->next[now] = new Node();
-			p->next[now]->flags = true;
-			p = p->next[now];
-		}
+		p = p->next[now];
 
+		++(p->cnt);
 		++str;
 	}
-	p->val = false;
+}
+
+bool findNode( char *str)
+{
+	int now;
+	Node *p = root;
+
+	while( *str )
+	{
+		now = *str - '0';
+		p = p->next[ now ];
+		if( p == NULL)
+			return true;
+		++str;
+	}
+	return ( p->cnt <= 1);
+}
+
+//Node *sta[MAXM];
+//int top = 0;
+
+stack<Node*> sta;
+
+void deleteNode( Node *now )
+{
+	map<int, Node*> :: iterator it;
+
+	sta.push( now );
+	while( !sta.empty() )
+	{
+		now = sta.top();
+		sta.pop();
+		for(it = now->next.begin(); it != now->next.end(); ++it)
+			sta.push(it->second);
+		delete now;
+	}
 }
 
 void solve()
 {
+	int T;
+	scanf("%d", &T);
+	while(T--)
+	{
+		int n;
+		root = new Node();
+		scanf("%d", &n);
+		for(int i = 0; i < n; ++i)
+		{
+			scanf("%s", s[i]);
+			insertNode( s[i] );
+		}
+
+		bool ans = true;
+		for(int i = 0; i < n && ans; ++i)
+		{
+			ans = findNode( s[i] );
+		}
+		puts( ans ? "YES" : "NO");
+
+		deleteNode( root );
+	}
 }
 
 int main()
