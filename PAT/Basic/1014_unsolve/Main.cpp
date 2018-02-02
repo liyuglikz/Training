91365fc14bbe662ce16253d32704628a6ac42a04
Texas-C <616041132@qq.com>
2018-02-02 21:35:40 +0800

    B-1014 can not solve >.<

--- PAT/Basic/1014_unsolve/Main.cpp
+++ PAT/Basic/1014_unsolve/Main.cpp
@@ -7,6 +7,11 @@ char ans[][10] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 vector<char> lib;
 int flag;
 
+bool judgeC( char c)
+{
+	return (c >= 'A' && c <= 'G');
+}
+
 bool judgec(char c)
 {
 	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
@@ -14,29 +19,19 @@ bool judgec(char c)
 
 bool judge( char c )
 {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' || c <= '9');
+	return (c >= 'A' && c <= 'N') || (c >= '0' || c <= '9');
 }
 
 int func_time( char c)
 {
 	if( c >= '0' && c <= '9')
-	{
 		return c - '0';
-	}
-	else if(c >= 'A' && c <= 'Z')
-	{
-		c += 32;
-	}
-	return (c - 'a') + 10;
+	return (c - 'A') + 10;
 }
 
 int func_day( char c)
 {
-	if(c >= 'A' && c <= 'Z')
-	{
-		c += 32;
-	}
-	return (c - 'a');
+	return (c - 'A');
 }
 
 void cmp( char *s1, char *s2)
@@ -47,30 +42,22 @@ void cmp( char *s1, char *s2)
 	{
 		if( *(s1 + i) == *(s2 + i))
 		{
-			if( flag == 0 && judgec( *(s1 + i)) )
+			if( flag == 0 && judgeC( *(s1 + i)) )
 			{
 				lib.push_back( *(s1 + i) );
 				++flag;
 			}
-			else if(flag > 0 &&  judge( *(s1 + i) ) )
+			else if(flag == 1 &&  judge( *(s1 + i) ) )
+			{
 				lib.push_back( *(s1 + i) );
+				++flag;
+				return;
+			}
+			else if(flag == 2 && judgec(*(s1 +i)))
+				lib.push_back( (char)i );
 		}
 	}
 }
-
-int cmp2( char *s1, char *s2)
-{
-	int l1(strlen(s1)), l2(strlen(s2));
-	int l(MIN(l1, l2));
-	for(int i = 0; i < l; ++i)
-	{
-		if( (*(s1 + i) == *(s2 + i))  && judgec( *(s1 + i)))
-		{
-			return i;
-		} 
-	}
-}
-
 char s1[100], s2[100];
 
 int main()
@@ -78,7 +65,9 @@ int main()
 	scanf("%s%s", s1, s2);
 	cmp(s1, s2);
 	scanf("%s%s", s1, s2);
+	cmp(s1, s2);
+
 	printf("%s", ans[ func_day( lib[0])]);
 	printf(" ");
-	printf("%02d:%02d\n", func_time(lib[1]), cmp2(s1, s2));
+	printf("%02d:%02d\n", func_time(lib[1]) % 24, (int) lib[2]);
 }
